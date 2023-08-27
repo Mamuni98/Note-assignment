303862e9d14d15a822e4e562619d45b622943893
Mamuni98 <nabanitasahoo468@gmail.com>
2023-08-27 13:30:21 +0530

    filter-Subject

--- src/components/NoteFilter/NoteFilter.js
+++ src/components/NoteFilter/NoteFilter.js
@@ -1,18 +1,24 @@
 import classes from "./NoteFilter.module.css";
-
+import { useState } from "react";
 const NoteFilter = () => {
+  const [subject, setSubject] = useState("Physics");
+  const filterSubjectHandler = (event) => {
+    event.preventDefault();
+    setSubject(event.target.value);
+  };
+  console.log(subject);
   return (
     <div className={classes.filter}>
-      <div className={classes.filtercontrol}>
+      <form className={classes.filtercontrol}>
         <label>Filter by Subject</label>
-        <select>
+        <select value={subject} onChange={filterSubjectHandler}>
           <option value="Physics">Physics</option>
           <option value="Chemistry">Chemistry</option>
           <option value="Mathematics">Mathematics</option>
           <option value="English">English</option>
           <option value="Biology">Biology</option>
         </select>
-      </div>
+      </form>
     </div>
   );
 };
