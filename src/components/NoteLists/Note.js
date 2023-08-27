eb8c6d5102ea9b1486e69f21ca38bb15faf9c0bf
Mamuni98 <nabanitasahoo468@gmail.com>
2023-08-27 16:53:29 +0530

    final-debugging

--- src/components/NoteLists/Note.js
+++ src/components/NoteLists/Note.js
@@ -6,25 +6,33 @@ const Note = (props) => {
   const dispatch = useDispatch();
 
   const editNoteHandler = () => {
-   dispatch(showFormActions.editNote(props));
+    const editData = {
+      id: props.id,
+      title: props.title,
+      subject: props.subject,
+      notes: props.notes,
+    };
+    dispatch(showFormActions.editNote(editData));
     dispatch(showFormActions.showForm());
   };
   const deleteNoteHandler = () => {
     dispatch(showFormActions.deleteNote(props.id));
-  }
+  };
   const readNoteHandler = () => {
     const read = {
       title: props.title,
       notes: props.notes,
-    }
-    dispatch(showFormActions.addNoteRead(read))
-  }
+    };
+    dispatch(showFormActions.addNoteRead(read));
+  };
   return (
     <li className={classes.list}>
-      <Link to={`/${props.id}`}><div className={classes.listcontent} onClick={readNoteHandler}>
-        <h3>{props.title}</h3>
-        <h4>{props.subject}</h4>
-      </div></Link>
+      <Link to={`/${props.id}`}>
+        <div className={classes.listcontent} onClick={readNoteHandler}>
+          <h3>{props.title}</h3>
+          <h4>{props.subject}</h4>
+        </div>
+      </Link>
       <div className={classes.listbtns}>
         <button onClick={editNoteHandler}>Edit</button>
         <button onClick={deleteNoteHandler}>Delete</button>
